f72baf2924b02f98be95382eccea1ca70fbb296a
Kousuke Masu <118793682+KousukeMasu@users.noreply.github.com>
2023-09-27 13:17:32 +0900

    first commit

---
+++ src/components/task/TaskCard.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import TaskCardTitle from "./TaskCardTitle";
+import TaskCardDeleteButton from "./button/TaskCardDeleteButton";
+import TaskAddInput from "./input/TaskAddInput";
+import Tasks from "./Tasks";
+
+const TaskCard = () => {
+  const [inputText, setInputText] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  return (
+    <div className="taskCard">
+      <TaskCardTitle />
+      <TaskCardDeleteButton />
+      <TaskAddInput
+        inputText={inputText}
+        setInputText={setInputText}
+        taskList={taskList}
+        setTaskList={setTaskList}
+      />
+      <Tasks />
+    </div>
+  );
+};
+
+export default TaskCard;
---
+++ src/components/task/input/TaskAddInput.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const TaskAddInput = ({ inputText, setInputText, taskList, setTaskList }) => {
+  const handleSubmit = (e) => {
+    // e.preventDefault();
+    console.log(e);
+  };
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+    console.log(inputText);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="add a task"
+          className="taskAddInput"
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default TaskAddInput;
